4e8088bb115dee3d06a124221a9a79f2ae65d6a1
EsadSimitcioglu <esat-muhammet@hotmail.com>
2020-05-30 23:25:29 +0300

    Final version of the code

--- src/client/Main.java
+++ src/client/Main.java
@@ -1,16 +1,9 @@
-/**
- * Need to change all of the tables -> make as HW2
- * Need to change insert in website
- */
 
 package client;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.nio.CharBuffer;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -18,81 +11,87 @@ import connection.DBConnection;
 
 public class Main {
 
-//	public static void instantiateJDBC() {
-//		try {
-//			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-//		} catch (InstantiationException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IllegalAccessException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (ClassNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	}
+	public static void instantiateJDBC() {
+        try {
+             Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql?zeroDateTimeBehavior=convertToNull",
+                    "root", "root");
+            Statement s = connection.createStatement();
+            int r = s.executeUpdate("create database if not exists web_crawler");
+            s.executeUpdate("use web_crawler");
+
+
+
+        } catch (InstantiationException e) {
+
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 
     public static void processCommand(DBConnection connection, String[] args) throws SQLException {
-        if (args.length == 2) {
+        if (args.length == 1) {
             // it must be check command
-            if (args[1].equalsIgnoreCase("check")) {
-                // TODO implement your logic here
-
-                if(connection.check_connection())
-                    System.out.println("Connection established");
+            if (args[0].equalsIgnoreCase("check")) {
+                if(connection.isConnected())
+                    System.out.println("connection established");
                 else
-                    System.out.println("Connection failed");
+                    System.out.println("connection failed");
 
             } else {
-                System.out.println("Unexpected command!");
+                System.out.println("Unexpected command1!");
             }
 
 
-        } else if (args.length == 3) {
+        } else if (args.length == 2) {
             // it can be 'add products' or 'query'
-            if (args[1].equalsIgnoreCase("add") && args[2].equalsIgnoreCase("products")) {
+            if (args[0].equalsIgnoreCase("add") && args[1].equalsIgnoreCase("products")) {
                 Scanner scanner = new Scanner(System.in);
-                boolean check = true;
+                ArrayList<String> lines = new ArrayList<>();
                 ArrayList<Integer> ids = new ArrayList<>();
-
-
-                while(scanner.hasNextLine()) {
+                while (true) {
                     String line = scanner.nextLine();
-                    String tempId = "";
 
-                    for(int i = 0; i < line.length(); i++) {
-                        if(line.charAt(i) == ',')
-                            break;
-                        else
-                            tempId += line.charAt(i);
+                    if (line.isEmpty())
+                        break;
+
+                    lines.add(line);
+                }
+                for (int i = 0 ; i < lines.size() ; i ++) {
+
+                    ids.add(Integer.parseInt(lines.get(i).split(",")[0]));
+
+                }
+                int firstGarbage = 0;
+                boolean check = true;
+                for (int j = 0 ; j < lines.size() ; j++) {
+
+                    check = connection.insertData("product", lines.get(j));
+
+                    if (!check) {
+                        firstGarbage = j;
+                        break;
                     }
 
-                    if(!scanner.hasNextLine()) {
-                        line = line.substring(0,line.length() - 5);
-                    }
+                }
 
-                    check = connection.insert_data("product",line);
-
-                    if(check)
-                        ids.add(Integer.parseInt(tempId));
-                    else  {
-                        for(int i = 0; i < ids.size(); i++) {
-                            System.out.println(ids.get(i));
-                            connection.delete_data("product",String.valueOf(ids.get(i)));
-                        }
-                        System.out.println("Bulk data cannot be processed");
-                        return;
-                    }
+                for (int k = 0; k < firstGarbage ; k ++) {
+                    connection.deleteData("product",String.valueOf(ids.get(k)));
                 }
 
 
 
 
-
-            } else if (args[1].equalsIgnoreCase("query")) {
-                if(args[2].equalsIgnoreCase("1")) {
+            } else if (args[0].equalsIgnoreCase("query")) {
+                if(args[1].equalsIgnoreCase("1")) {
                     // Find the most popular product in all websites. I.E., the product sold in the highest number of websites
 
                     ResultSet set = connection.send_query("SELECT P.name\n" +
@@ -105,10 +104,20 @@ public class Main {
                             "\t\t\t  );");
 
                     while(set.next()) {
-                        System.out.println(set.getString("name"));
+                        System.out.println(set.getString("Id") + "," + set.getString("name") + "," + set.getString("description") + "," + set.getString("brandname"));
                     }
                 }
-                else if(args[2].equalsIgnoreCase("3")){
+                else if(args[1].equalsIgnoreCase("2")){
+                    ResultSet set = connection.send_query("select S.PID,S.WURL,S.date\n" +
+                            "from sell S\n" +
+                            "group by S.PID,S.WURL\n" +
+                            "order by  S.PID asc , S.Initial_price-S.Discounted_price asc ; ");
+
+                    while(set.next()) {
+                        System.out.println(set.getString("PID") + "," + set.getString("WURL") + "," + set.getString("Date"));
+                    }
+                }
+                else if(args[1].equalsIgnoreCase("3")) {
                     ResultSet set = connection.send_query("select *\n" +
                             "from product P\n" +
                             "where NOT EXISTS (select S.PID\n" +
@@ -116,69 +125,71 @@ public class Main {
                             "\t\t\t\t where S.PID = P.ID); ");
 
                     while(set.next()) {
-                        System.out.println(set.getString("Id") + "|" + set.getString("name") + "|" + set.getString("description") + "|" + set.getString("brandname") );
+                        System.out.println(set.getString("Id") + "," + set.getString("name") + "," + set.getString("description") + "," + set.getString("brandname") );
                     }
                 }
 
+
             } else {
-                System.out.println("Unexpected command!");
+                System.out.println("Unexpected command2!");
             }
         }
 
-        else if (args.length == 4) {
-            if (args[1].equalsIgnoreCase("add")) {
-                if (args[2].equalsIgnoreCase("product")) {
-                    connection.insert_data("product",args[3]);
+        else if (args.length == 3) {
+            if (args[0].equalsIgnoreCase("add")) {
+                if (args[1].equalsIgnoreCase("product")) {
+                    connection.insertData("product",args[2]);
 
+                } else if (args[1].equalsIgnoreCase("productKeyword")) {
+                    connection.insertData("productKeyword", args[2]);
 
-                } else if (args[2].equalsIgnoreCase("productKeyword")) {
-                    connection.insert_data("productKeyword", args[3]);
+                } else if (args[1].equalsIgnoreCase("website")) {
+                    connection.insertData("website", args[2]);
 
-                } else if (args[2].equalsIgnoreCase("website")) {
-                    connection.insert_data("website", args[3]);
+                } else if (args[1].equalsIgnoreCase("websitephone")) {
+                    connection.insertData("websitephone", args[2]);
 
-                } else if (args[2].equalsIgnoreCase("websitephone")) {
-                    connection.insert_data("websitephone", args[3]);
+                } else if (args[1].equalsIgnoreCase("externalsupplier")) {
+                    connection.insertData("externalsupplier", args[2]);
 
-                } else if (args[2].equalsIgnoreCase("externalsupplier")) {
-                    connection.insert_data("externalsupplier", args[3]);
-
-                } else if (args[2].equalsIgnoreCase("sell")) {
-                    connection.insert_data("sell", args[3]);
+                } else if (args[1].equalsIgnoreCase("sell")) {
+                    connection.insertData("sell", args[2]);
                 } else {
-                    System.out.println("Unexpected command!");
+                    System.out.println("Unexpected command3!");
                 }
 
-            } else if(args[1].equalsIgnoreCase("delete")) {
-                if (args[2].equalsIgnoreCase("product")) {
-                    connection.delete_data("product",args[3]);
+            } else if(args[0].equalsIgnoreCase("delete")) {
+                if (args[1].equalsIgnoreCase("product")) {
+                    connection.deleteData("product",args[2]);
 
-                } else if (args[2].equalsIgnoreCase("productKeyword")) {
-                    connection.delete_data("productKeyword",args[3]);
-                } else if (args[2].equalsIgnoreCase("website")) {
-                    // TODO implement your logic here
-                } else if (args[2].equalsIgnoreCase("websitephone")) {
-                    // TODO implement your logic here
-                } else if (args[2].equalsIgnoreCase("externalsupplier")) {
-                    // TODO implement your logic here
-                } else if (args[2].equalsIgnoreCase("sell")) {
-                    // TODO implement your logic here
+                } else if (args[1].equalsIgnoreCase("productKeyword")) {
+                    connection.deleteData("productKeyword",args[2]);
+                } else if (args[1].equalsIgnoreCase("website")) {
+                    connection.deleteData("website",args[2]);
+                } else if (args[1].equalsIgnoreCase("websitephone")) {
+                    connection.deleteData("websitephone",args[2]);
+                } else if (args[1].equalsIgnoreCase("externalsupplier")) {
+                    connection.deleteData("externalsupplier",args[2]);
+                } else if (args[1].equalsIgnoreCase("sell")) {
+                    connection.deleteData("sell",args[2]);
                 } else {
-                    System.out.println("Unexpected command!");
+                    System.out.println("Unexpected command4!");
                 }
 
             } else {
-                System.out.println("Unexpected command!");
+                System.out.println("Unexpected command5!");
             }
 
         } else {
-            System.out.println("Unexpected command!");
+            System.out.println("Unexpected command6!");
         }
     }
 
 
     public static String readConfig() throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\Esat\\IdeaProjects\\untitled2\\src\\properties.conf"));
+        InputStream inputStream =  Main.class.getResourceAsStream("/properties.conf");
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader reader = new BufferedReader(inputStreamReader);
         try {
             String ip = reader.readLine();
             String port = reader.readLine();
@@ -189,7 +200,7 @@ public class Main {
                     ip, port, username, password);
             return url;
         } catch (IOException e) {
-            // TODO Auto-generated catch block
+
             e.printStackTrace();
         } finally {
             reader.close();
@@ -198,16 +209,16 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
-        String[] arguments = {"1", "query","3"};
+        //String[] arguments = {"add","sell","2,something5,1999-10-10 10:10:10,25,5"};  //  deneme
 
 
         DBConnection database_connection = null;
 
-//		instantiateJDBC();
+		instantiateJDBC();
 
-//		if(args.length < 2) {
-//			System.err.println("Wrong number of arguments!");
-//		}
+		if(args.length < 1) {
+			System.err.println("Wrong number of arguments!");
+		}
 
         String url = readConfig();
 
